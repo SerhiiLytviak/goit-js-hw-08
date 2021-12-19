8fffa0d15df2d58e9af8c2ed88af044a3fa0cb82
SerhiiLytviak <litvyak.sergey@gmail.com>
2021-12-19 12:15:57 +0200

    Убран лишний код
    Убран лишний код из метода setCurrentTime()

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,17 +9,4 @@ const onPlay = function (timeupdate) {
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-  .then(function (seconds) {})
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
