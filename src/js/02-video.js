2c58352b008ef43ad5f8446eb785c82fb40a6db2
SerhiiLytviak <litvyak.sergey@gmail.com>
2021-12-19 11:53:33 +0200

    Сменил ивент в методе player.on
    Сменил ивент с 'play' на 'timeupdate'

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,13 @@
 import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 const onPlay = function (timeupdate) {
   localStorage.setItem('videoplayer-current-time', timeupdate.seconds);
 };
 
-player.on('play', throttle(onPlay, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
 
 player
   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
